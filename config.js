a48653fddcdd1cbfde671662789a07dfa065844c
pinchafede <federico.diaz.ilm@gmail.com>
2026-03-25 03:11:20 -0300

    Update config.js

--- config.js
+++ config.js
@@ -7,16 +7,16 @@ const CONFIG = {
 
   // URL de tu proyecto Supabase
   // Encontrala en: Project Settings → API → Project URL
-  SUPABASE_URL: "",
+  SUPABASE_URL: "https://pdsbbugvdnfbcrmqztwl.supabase.co",
 
   // Clave pública (anon key) de Supabase — es segura para el frontend
   // Encontrala en: Project Settings → API → anon public
-  SUPABASE_KEY: "",
+  SUPABASE_KEY: "39553606Fede!",
 
   // Contraseña del panel de admin
-  ADMIN_PASSWORD: "viernes2025",
+  ADMIN_PASSWORD: "Admin1234",
 
   // Temporada actual (solo visual)
-  SEASON: "2025",
+  SEASON: "2026",
 
 };

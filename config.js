a95835668f3c2ca80ff8bd8d095c8e83cef3bdaa
pinchafede <federico.diaz.ilm@gmail.com>
2026-03-25 02:21:19 -0300

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const CONFIG = {
 
   // ID de tu Google Sheet (lo encontrás en la URL de la planilla)
   // Ejemplo: https://docs.google.com/spreadsheets/d/AQUI_VA_EL_ID/edit
-  SHEET_ID: "https://docs.google.com/spreadsheets/d/1mlmpcrHfn8lTU2D9s6TZJJ5RI_OLoJHVagZmBTgxYK0/edit?hl=es&gid=0#gid=0",
+  SHEET_ID: "1mlmpcrHfn8lTU2D9s6TZJJ5RI_OLoJHVagZmBTgxYK0",
 
   // API Key de Google Cloud (Google Sheets API)
   // Ver instrucciones en la pestaña "Setup" de la app

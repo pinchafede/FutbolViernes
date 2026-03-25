3a2f41061a66877bd4f4e799076d8c69c1125c12
pinchafede <federico.diaz.ilm@gmail.com>
2026-03-25 02:13:54 -0300

    Update config.js
    configuraciones iniciales

--- config.js
+++ config.js
@@ -7,13 +7,13 @@ const CONFIG = {
 
   // ID de tu Google Sheet (lo encontrás en la URL de la planilla)
   // Ejemplo: https://docs.google.com/spreadsheets/d/AQUI_VA_EL_ID/edit
-  SHEET_ID: "",
+  SHEET_ID: "https://docs.google.com/spreadsheets/d/1mlmpcrHfn8lTU2D9s6TZJJ5RI_OLoJHVagZmBTgxYK0/edit?hl=es&gid=0#gid=0",
 
   // API Key de Google Cloud (Google Sheets API)
   // Ver instrucciones en la pestaña "Setup" de la app
-  API_KEY: "",
+  API_KEY: "AIzaSyBiNLOZIXqu_C_m70JqlNsmrDnkgbEoJ5g",
 
   // Nombre de la temporada (aparece en el header)
-  SEASON: "2025",
+  SEASON: "2026",
 
 };
